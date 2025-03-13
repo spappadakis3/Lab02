99c3e94f408ed32519c9efd377581f84971e03fa
spappadakis3 <spappadakis3@gmail.com>
2025-03-13 14:32:25 -0400

    Home_Page.py

--- Home_Page.py
+++ Home_Page.py
@@ -8,10 +8,6 @@ st.subheader("My name is Sophia Pappadakis and I am currently taking CS 1301")
 st.write("Portfolio: This page contains my portfolio")
 st.write("About me: This page contains information about me!")
 
-st.page_link("Home_Page.py", label="Home", icon="🏠")
-st.page_link("pages/portfolio.py", label="Portfolio", icon="1️⃣")
-st.page_link("pages/Time Management Tracker.py", label="About Me", icon="2️⃣")
-st.page_link("http://www.google.com", label="Google", icon="🌎")
 
 
     
