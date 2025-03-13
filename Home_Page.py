b51d19df848af61fa0097e3df77d69c05bece376
spappadakis3 <spappadakis3@gmail.com>
2025-03-13 14:43:19 -0400

    Home_Page.py

--- Home_Page.py
+++ Home_Page.py
@@ -6,7 +6,7 @@ import streamlit as st
 st.header("Home Page")
 st.subheader("My name is Sophia Pappadakis and I am currently taking CS 1301")
 st.write("Portfolio: This page contains my portfolio")
-st.write("About me: This page contains information about me!")
+st.write("Time Management Tracker: This page contains a tracker for you to document your time spent this week!")
 
 
 
